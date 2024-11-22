5529c31ef9a002e05c3a506a262d274fe12fbe82
Kalpit Gudigar <153449924+KalpitG-gh@users.noreply.github.com>
2024-11-22 13:20:50 +0530

    Update Controller.java

--- my-java-project/backend/src/main/java/com/example/demo/Controller.java
+++ my-java-project/backend/src/main/java/com/example/demo/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
     @GetMapping("/greeting")
     public Greeting greet(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting("Hello, " + name + "!");
